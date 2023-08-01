464532ba1888f4353c3514743ef2d7934e090b49
aashayk18 <aashayk100@gmail.com>
2023-08-01 11:55:00 +0530

    added owner

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/add-pet', (req, res) => {
 
     try {
       await s3.putObject(params).promise();
-      petData.image.url = `https:/d35c9z5id0no27.cloudfront.net/${fileName}`;
+      petData.image.url = `https://d35c9z5id0no27.cloudfront.net/${fileName}`;
       console.log('File uploaded successfully');
     } catch (error) {
       console.error(error);
